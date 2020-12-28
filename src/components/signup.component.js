0523002a211b3d0d9e4bf3d7765dcc14d2ec07c8
nguyendatils <61770569+nguyendatils@users.noreply.github.com>
2020-12-28 21:52:52 +0700

    fix signup UI

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -5,8 +5,8 @@ import '../css/signup.css';
 export default class Signup extends Component {
     render() {
         return(
-            <div className="signup-layout">
-                <nav className="navbar navbar-dark navbar-expand-lg">
+            <div className="">
+                <nav className="navbar navbar-dark navbar-expand-lg user-nav">
                     <div className="container">
                         <Link to="/" className="navbar-brand">EasyAccomod</Link>
                         <ul className="navbar-nav navbar-right">
@@ -18,15 +18,34 @@ export default class Signup extends Component {
                         </ul>
                     </div>
                 </nav>
-                <h1 className="text-light slogan">Register an account to access the website</h1>
-                <div className="link-signup">
-                    <div className="text-center row">
-                        <div className="col-sm-6">
-                            <Link to="/signup/renter"className="d-block btn btn-outline-light float-right" >For Renter</Link></div>
-                        <div className="col-sm-6">
-                            <Link to="/signup/owner" className="d-block btn btn-outline-light float-left" >For Owner</Link></div>
-                    </div>   
+                <br/>
+                <br/>
+                <br/>
+                <div className="form-container col-md-6 col-lg-5 col-sm-7 col-xl-4">
+                    <br/>
+                    <div className="form-block col-sm-11">
+                        <div className="">
+                            <div className="text-center">
+                                <img src="../../../home.ico" alt="logo" className="logo"/>
+                                <h2 className="text-info">EasyAccomod</h2>
+                            </div>
+                        </div>
+                        <br/>
+                        <p className="slogan">EasyAccomod is much better when you have an account </p>
+                        <h3 className="text-center">Sign Up Now</h3>
+                        <br/>
+                        <div className="link-signup">
+                            <div className="text-center row">
+                                <div className="text-center col-sm-6">
+                                    <Link to="/signup/renter"className="d-block btn btn-info" >For Renter</Link></div>
+                                <div className="text-center col-sm-6">
+                                    <Link to="/signup/owner" className="d-block btn btn-info" >For Owner</Link></div>
+                            </div>   
+                        </div>
+                    </div>
                 </div>
+                <h1 className="text-light slogan">Register an account to access the website</h1>
+                
             </div>
         );
     }
